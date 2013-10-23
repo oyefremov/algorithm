2d8855297b14c88efaa92002042c16a6fe3fe9bc
Oleksandr Yefremov <oleksandr.yefremov@gmail.com>
2013-10-23 21:43:12 +0300

    foreach algorithms

--- include/boost/algorithm/cxx11/detail/foreach.hpp
+++ include/boost/algorithm/cxx11/detail/foreach.hpp
@@ -21,62 +21,70 @@ namespace boost { namespace algorithm { namespace detail {
 		return prev_if_bidirectional_iterator(i, iterator_category<ForwardIterator>());
 	}
 
-	template <class InputIterator, class F>
-	void for_each_iterator(InputIterator begin, InputIterator end, F f) {
-		for ( ; begin != end; ++begin )
+	template <class InputIterator, class Function>
+	void for_each_iterator(InputIterator begin, InputIterator end, Function f) {
+		for (; begin != end; ++begin)
 			f(const_cast<const InputIterator&>(begin));
 	}
 
-#if 0 // std::for_each is preffered
-	template <class InputIterator, class F>
-	void for_each(InputIterator begin, InputIterator end, F f) {
+	template <class T> const T& add_const(T& a) { return a; }
+
+#if 0 // use std::for_each instead
+	template <class InputIterator, class Function>
+	void for_each(InputIterator begin, InputIterator end, Function f) {
 		for_each_iterator(begin, end, [&f](InputIterator i){ f(*i); });
 	}
 #endif
 
-	template <class ForwardIterator, class F>
-	void for_each_pair_iterator(ForwardIterator begin, ForwardIterator end, F f) {
+	template <class ForwardIterator, class Function>
+	void for_each_pair_iterator(ForwardIterator begin, ForwardIterator end, Function f) {
 		if (begin == end) return;
 		const ForwardIterator before_end = prev_if_bidirectional_iterator(end);
 		for_each_iterator(begin, before_end, [&](ForwardIterator i) {
-			for_each_iterator(std::next(i), end, [&](ForwardIterator j) { f(i, j); });
+			for_each_iterator(std::next(i), end, [&](ForwardIterator j) { 
+				f(add_const(i), add_const(j));
+			});
 		});
 	}
 
-	template <class ForwardIterator, class F>
-	void for_each_triple_iterator(ForwardIterator begin, ForwardIterator end, F f) {
+	template <class ForwardIterator, class Function>
+	void for_each_triple_iterator(ForwardIterator begin, ForwardIterator end, Function f) {
 		if (begin == end) return;
 		const ForwardIterator before_end = prev_if_bidirectional_iterator(end);
 		for_each_pair_iterator(begin, before_end, [&](ForwardIterator i, ForwardIterator j) {
-			for_each_iterator(std::next(j), end, [&](ForwardIterator k) { f(i, j, k); });
+			for_each_iterator(std::next(j), end, [&](ForwardIterator k) { 
+				f(add_const(i),	add_const(j), add_const(k));
+			});
 		});
 	}
 
-	template <class ForwardIterator, class F>
-	void for_each_quad_iterator(ForwardIterator begin, ForwardIterator end, F f) {
+	template <class ForwardIterator, class Function>
+	void for_each_quad_iterator(ForwardIterator begin, ForwardIterator end, Function f) {
 		if (begin == end) return;
 		const ForwardIterator before_end = prev_if_bidirectional_iterator(end);
 		for_each_triple_iterator(begin, before_end, [&](ForwardIterator i, ForwardIterator j, ForwardIterator k) {
-			for_each_iterator(std::next(k), end, [&](ForwardIterator l) { f(i, j, k, l); });
+			for_each_iterator(std::next(k), end, [&](ForwardIterator l) { 
+				f(add_const(i), add_const(j), add_const(k), add_const(l));
+			});
 		});
 	}
 
-	template <class InputIterator, class F>
-	InputIterator find_if_iterator(InputIterator begin, InputIterator end, F f) {
+	template <class InputIterator, class Predicate>
+	InputIterator find_if_iterator(InputIterator begin, InputIterator end, Predicate f) {
 		for (; begin != end; ++begin)
-			if (f(begin)) 
+			if (f(add_const(begin)))
 				break;
 		return begin;
 	}
 
-	template <class ForwardIterator, class F>
-	std::pair<ForwardIterator, ForwardIterator> find_pair_if_iterator(ForwardIterator begin, ForwardIterator end, F f) {
+	template <class ForwardIterator, class Predicate>
+	std::pair<ForwardIterator, ForwardIterator> find_pair_if_iterator(ForwardIterator begin, ForwardIterator end, Predicate f) {
 		ForwardIterator i0 = end, i1 = end;
 		if (begin != end) {
 			const ForwardIterator before_end = prev_if_bidirectional_iterator(end);
 			i0 = find_if_iterator(begin, before_end, [&](ForwardIterator i) -> bool {
 				i1 = find_if_iterator(next(i), end, [&](ForwardIterator j){
-					return f(i, j);
+					return f(add_const(i), add_const(j));
 				});
 				return end != i1;
 			});
@@ -84,14 +92,14 @@ namespace boost { namespace algorithm { namespace detail {
 		return std::make_pair(i0, i1);
 	}
 
-	template <class ForwardIterator, class F>
-	std::tuple<ForwardIterator, ForwardIterator, ForwardIterator> find_triple_if_iterator(ForwardIterator begin, ForwardIterator end, F f) {
+	template <class ForwardIterator, class Predicate>
+	std::tuple<ForwardIterator, ForwardIterator, ForwardIterator> find_triple_if_iterator(ForwardIterator begin, ForwardIterator end, Predicate f) {
 		ForwardIterator i0 = end, i1 = end, i2 = end;
 		if (begin != end) {
 			const ForwardIterator before_end = prev_if_bidirectional_iterator(end);
 			i0 = find_if_iterator(begin, before_end, [&](ForwardIterator i) -> bool {
 				std::tie(i1, i2) = find_pair_if_iterator(next(i), end, [&](ForwardIterator j, ForwardIterator k){
-					return f(i, j, k);
+					return f(add_const(i), add_const(j), add_const(k));
 				});
 				return end != i2;
 			});
@@ -99,14 +107,14 @@ namespace boost { namespace algorithm { namespace detail {
 		return std::make_tuple(i0, i1, i2);
 	}
 
-	template <class ForwardIterator, class F>
-	std::tuple<ForwardIterator, ForwardIterator, ForwardIterator, ForwardIterator> find_quad_if_iterator(ForwardIterator begin, ForwardIterator end, F f) {
+	template <class ForwardIterator, class Predicate>
+	std::tuple<ForwardIterator, ForwardIterator, ForwardIterator, ForwardIterator> find_quad_if_iterator(ForwardIterator begin, ForwardIterator end, Predicate f) {
 		ForwardIterator i0 = end, i1 = end, i2 = end, i3 = end;
 		if (begin != end) {
 			const ForwardIterator before_end = prev_if_bidirectional_iterator(end);
 			i0 = find_if_iterator(begin, before_end, [&](ForwardIterator i) -> bool {
 				std::tie(i1, i2, i3) = find_triple_if_iterator(next(i), end, [&](ForwardIterator j, ForwardIterator k, ForwardIterator l){
-					return f(i, j, k, l);
+					return f(add_const(i), add_const(j), add_const(k), add_const(l));
 				});
 				return end != i3;
 			});
--- include/boost/algorithm/cxx11/foreach.hpp
+++ include/boost/algorithm/cxx11/foreach.hpp
@@ -6,24 +6,24 @@
 
 namespace boost { namespace algorithm { 
 
-	template <class ForwardIterator, class F>
-	void for_each_pair(ForwardIterator begin, ForwardIterator end, F f) {
+	template <class ForwardIterator, class Function>
+	void for_each_pair(ForwardIterator begin, ForwardIterator end, Function f) {
 		detail::for_each_pair_iterator(begin, end, [&f](ForwardIterator i, ForwardIterator j) { f(*i, *j); });
 	}
 
-	template <class ForwardIterator, class F>
-	void for_each_triple(ForwardIterator begin, ForwardIterator end, F f) {
+	template <class ForwardIterator, class Function>
+	void for_each_triple(ForwardIterator begin, ForwardIterator end, Function f) {
 		detail::for_each_triple_iterator(begin, end, [&f](ForwardIterator i, ForwardIterator j, ForwardIterator k) { f(*i, *j, *k); });
 	}
 
-	template <class ForwardIterator, class F>
-	void for_each_quad(ForwardIterator begin, ForwardIterator end, F f) {
+	template <class ForwardIterator, class Function>
+	void for_each_quad(ForwardIterator begin, ForwardIterator end, Function f) {
 		detail::for_each_quad_iterator(begin, end, [&f](ForwardIterator i, ForwardIterator j, ForwardIterator k, ForwardIterator l) { f(*i, *j, *k, *l); });
 	}
 
-	template <class ForwardIterator, class F, class result_t>
-	result_t count_pair_if(ForwardIterator begin, ForwardIterator end, F f) {
-		result_t result = 0;
+	template <class ForwardIterator, class Predicate>
+	size_t count_pair_if(ForwardIterator begin, ForwardIterator end, Predicate f) {
+		size_t result = 0;
 		for_each_pair(begin, end, [&f](const ForwardIterator::value_type& i, const ForwardIterator::value_type& j) { 
 			if (f(i, j))
 				++result; 
@@ -31,9 +31,9 @@ namespace boost { namespace algorithm {
 		return result;
 	}
 		
-	template <class ForwardIterator, class F, class result_t>
-	result_t count_triple_if(ForwardIterator begin, ForwardIterator end, F f) {
-		result_t result = 0;
+	template <class ForwardIterator, class Predicate>
+	size_t count_triple_if(ForwardIterator begin, ForwardIterator end, Predicate f) {
+		size_t result = 0;
 		for_each_triple(begin, end, [&](const ForwardIterator::value_type& i, const ForwardIterator::value_type& j, const ForwardIterator::value_type& k) {
 			if (f(i, j, k))
 				++result;
@@ -41,9 +41,9 @@ namespace boost { namespace algorithm {
 		return result;
 	}
 		
-	template <class ForwardIterator, class F, class result_t>
-	result_t count_quad_if(ForwardIterator begin, ForwardIterator end, F f) {
-		result_t result = 0;
+	template <class ForwardIterator, class Predicate>
+	size_t count_quad_if(ForwardIterator begin, ForwardIterator end, Predicate f) {
+		size_t result = 0;
 		for_each_quad(begin, end, [&](const ForwardIterator::value_type& i, const ForwardIterator::value_type& j, const ForwardIterator::value_type& k, const ForwardIterator::value_type& l) {
 			if (f(i, j, k, l))
 				++result;
@@ -51,22 +51,22 @@ namespace boost { namespace algorithm {
 		return result;
 	}
 	
-	template <class ForwardIterator, class F>
-	std::pair<ForwardIterator, ForwardIterator> find_pair_if(ForwardIterator begin, ForwardIterator end, F f) {
+	template <class ForwardIterator, class Predicate>
+	std::pair<ForwardIterator, ForwardIterator> find_pair_if(ForwardIterator begin, ForwardIterator end, Predicate f) {
 		return detail::find_pair_if_iterator(begin, end, [&f](ForwardIterator i0, ForwardIterator i1){ 
 			return f(*i0, *i1); 
 		});
 	}
 
-	template <class ForwardIterator, class F>
-	std::tuple<ForwardIterator, ForwardIterator, ForwardIterator> find_triple_if(ForwardIterator begin, ForwardIterator end, F f) {
+	template <class ForwardIterator, class Predicate>
+	std::tuple<ForwardIterator, ForwardIterator, ForwardIterator> find_triple_if(ForwardIterator begin, ForwardIterator end, Predicate f) {
 		return detail::find_triple_if_iterator(begin, end, [&f](ForwardIterator i0, ForwardIterator i1, ForwardIterator i2){ 
 			return f(*i0, *i1, *i2); 
 		});
 	}
 
-	template <class ForwardIterator, class F>
-	std::tuple<ForwardIterator, ForwardIterator, ForwardIterator, ForwardIterator> find_quad_if(ForwardIterator begin, ForwardIterator end, F f) {
+	template <class ForwardIterator, class Predicate>
+	std::tuple<ForwardIterator, ForwardIterator, ForwardIterator, ForwardIterator> find_quad_if(ForwardIterator begin, ForwardIterator end, Predicate f) {
 		return detail::find_triple_if_iterator(begin, end, [&f](ForwardIterator i0, ForwardIterator i1, ForwardIterator i2, ForwardIterator i3){ 
 			return f(*i0, *i1, *i2, *i3); 
 		});
